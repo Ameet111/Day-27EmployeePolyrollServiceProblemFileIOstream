56bcacea8773ac7733453e75a2a87b0f9cd6eec4
Ameet Jadhav <ammugs11@gmail.com>
2022-08-11 23:57:51 +0530

    added use case 1 for creating employee payroll service read and write operation

--- src/main/java/com/bridgelab/IOfile/App.java
+++ src/main/java/com/bridgelab/IOfile/App.java
@@ -1,5 +1,9 @@
 package com.bridgelab.IOfile;
 
-public class App {
-
+public class App 
+	{
+	    public static void main( String[] args )
+	    {
+	        System.out.println( "Welcome To Employee Payroll service" );
+	    }
 }
